86fab52e4afc07f7810062f00bdcf6725a6fccff
rwells <rwells02@qub.ac.uk>
2019-02-06 12:04:22 +0000

    end

--- main.py
+++ main.py
@@ -15,5 +15,16 @@ def load_csv(month):
     return df
 
 
-data = load_csv(user_input())
+# data = load_csv(user_input())
+data = load_csv("october")
 
+practices = set(data["Practice"])
+
+for practice_id in list(practices)[:1]:
+    practice_data = data[data["Practice"] == practice_id]
+
+    sorted_data = practice_data.sort_values("Number of Items", ascending=False)
+
+    cut_data = sorted_data[:5]
+
+    print cut_data
